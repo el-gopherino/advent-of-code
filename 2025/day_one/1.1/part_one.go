480ce79abe3f1a5a1db53d872ae7cb17b2b733f2
mdb-ampluggedWav <matdb.music@gmail.com>
2026-01-06 15:47:56 -0500

    2025: day 1, pt. 1

---
+++ 2025/day_one/1.1/part_one.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// https://adventofcode.com/2025/day/1
+
+// PART 1
+
+func calibrate(originalPosition, turns int) (result int) {
+	result = (originalPosition + turns) % 100
+	if result < 0 {
+		result += 100
+	}
+	return result
+}
+
+func calculate(currentLine string, currentPosition int) int {
+	rotationNum, _ := strconv.Atoi(currentLine[1:])
+
+	switch currentLine[0] {
+	case 'R':
+		return calibrate(currentPosition, rotationNum)
+	case 'L':
+		return calibrate(currentPosition, -rotationNum)
+	}
+
+	return currentPosition
+}
+
+func main() {
+
+	file, err := os.Open("day_one_input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// init scanner object to read file
+	scanner := bufio.NewScanner(file)
+
+	// split each scanned line into words
+	scanner.Split(bufio.ScanWords)
+
+	const startPosition = 50
+	var currentPosition = startPosition
+	var zeroes int
+
+	for scanner.Scan() {
+		currentLine := scanner.Text()
+		nextPos := calculate(currentLine, currentPosition)
+
+		if nextPos == 0 {
+			zeroes++
+			fmt.Printf("Found zero!!! zeroes encountered: %d\n", zeroes)
+		}
+		currentPosition = nextPos
+	}
+
+	fmt.Println(zeroes)
+
+}
