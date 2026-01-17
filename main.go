5be94770b5bf36dd7f04f2ff68753ef5edda4693
mdb-ampluggedWav <matdb.music@gmail.com>
2026-01-16 21:21:10 -0500

    day 2, pt.1 (2025)

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-type celsius float64
-
-type temperature struct {
-	celsius
-}
-
-func (c celsius) String() string {
-	return fmt.Sprintf("%.2f ºC", c)
-}
-
 func main() {
-	c := celsius(10.0)
-	fmt.Println(c)
-
-	t := temperature{c}
-	fmt.Println(t)
 
 }
